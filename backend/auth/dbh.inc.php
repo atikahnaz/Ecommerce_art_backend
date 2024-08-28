45cdd86a8aad17d5aed40fadbe64b1d5b9acaef4
Atikah <atikahnazir6@gmail.com>
2024-08-28 14:40:13 +0800

    connect to freesqldatabase

--- backend/auth/dbh.inc.php
+++ backend/auth/dbh.inc.php
@@ -1,8 +1,12 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=login_system";
-$dbusername = "root";
-$dbpassword = "";
+// $dsn = "mysql:host=localhost;dbname=login_system";
+// $dbusername = "root";
+// $dbpassword = "";
+
+$dsn = "mysql:host=sql12.freesqldatabase.com;dbname=sql12727880";
+$dbusername = "sql12727880";
+$dbpassword = "nUp9stpY6S";
 
 try {
     $pdo = new PDO($dsn,$dbusername,$dbpassword);
